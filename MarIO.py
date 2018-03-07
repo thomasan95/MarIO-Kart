7a82619fd4217ccd854acf192cb05bfe5d309b98
Thomas <thomasan1995@gmail.com>
2018-03-07 02:41:59 -0800

    bug with tf.graph

--- MarIO.py
+++ MarIO.py
@@ -4,12 +4,14 @@ import numpy as np
 import argparse
 import random
 import utilities as utils
+import gym
 print("Using TensorFlow version: " + str(tf.__version__))
 # Load Configs
 conf = config.Config()
 parser = argparse.ArgumentParser(description="specify task of the network")
 parser.add_argument("-t", "-task", type=str, default='train', help="will implement other parameters")
 args = parser.parse_args()
+env = gym.make('Acrobot-v1')
 
 
 def create_graph():
@@ -32,110 +34,101 @@ def create_graph():
     :return: graph, input_placeholder, max_actions placeholder, optimal_action, out, action, loss, optimizer
     :rtype: tf.Graph(), tf.placeholder(), tf.placeholder(), tf.placeholder(), tf.Tensor, tf.Tensor, loss, tf optimizer
     """
-    graph = tf.Graph()
-    with graph.as_default():
-        keep_prob = conf.keep_prob
-        # Fill in shape later since we'll downsample and resize
-        with tf.name_scope("Input"):
-            # Input
-            inp = tf.placeholder(tf.float32, shape=[None, conf.img_h, conf.img_w, conf.img_d], name="input")
-            # For storing what action to take output by the network
-            max_action = tf.placeholder(tf.float32, [None, conf.OUTPUT_SIZE], name="max_action")
-            # Placeholder for storing optimal action (labels)
-            optimal_action = tf.placeholder(tf.float32, [None], name="optimal_action")
+    keep_prob = conf.keep_prob
+    # Fill in shape later since we'll downsample and resize
+    with tf.variable_scope("actor"):
+        # actor = tf.Graph()
+        # with actor.as_graph_def():
+        with tf.variable_scope("actor_input"):
+            state_inp = tf.placeholder(tf.float32, shape=conf.inp_shape, name="actor_state_input")
+            actor_action = tf.placeholder(tf.float32, shape=env.action_space.n, name="actor_action_ph")
+        with tf.variable_scope("actor_kernels_weights"):
+            a_w1 = tf.get_variable(name="act_W1", shape=[5, 5, 3, 24],
+                                   initializer=tf.contrib.layers.xavier_initializer())
+            a_b1 = tf.get_variable(name="act_b1", shape=[24], initializer=tf.zeros_initializer)
+            a_w2 = tf.get_variable(name='act_W2', shape=[5, 5, 24, 36],
+                                   initializer=tf.contrib.layers.xavier_initializer())
+            a_b2 = tf.get_variable(name='act_b2', shape=[36], initializer=tf.zeros_initializer)
 
-        with tf.name_scope("Kernels_and_Bias"):
-            w1 = tf.get_variable(name='W1', shape=[5, 5, 3, 24], initializer=tf.contrib.layers.xavier_initializer())
-            b1 = tf.get_variable(name='b1', shape=[24], initializer=tf.zeros_initializer)
+            a_w3 = tf.get_variable(name='act_W3', shape=[5, 5, 36, 48],
+                                   initializer=tf.contrib.layers.xavier_initializer())
+            a_b3 = tf.get_variable(name='act_b3', shape=[48], initializer=tf.zeros_initializer)
 
-            w2 = tf.get_variable(name='W2', shape=[5, 5, 24, 36], initializer=tf.contrib.layers.xavier_initializer())
-            b2 = tf.get_variable(name='b2', shape=[36], initializer=tf.zeros_initializer)
+            a_w4 = tf.get_variable(name='act_W4', shape=[3, 3, 48, 64],
+                                   initializer=tf.contrib.layers.xavier_initializer())
+            a_b4 = tf.get_variable(name='act_b4', shape=[64], initializer=tf.zeros_initializer)
 
-            w3 = tf.get_variable(name='W3', shape=[5, 5, 36, 48], initializer=tf.contrib.layers.xavier_initializer())
-            b3 = tf.get_variable(name='b3', shape=[48], initializer=tf.zeros_initializer)
+            a_w5 = tf.get_variable(name='act_W5', shape=[3, 3, 64, 64],
+                                   initializer=tf.contrib.layers.xavier_initializer())
+            a_b5 = tf.get_variable(name='act_b5', shape=[64], initializer=tf.zeros_initializer)
 
-            w4 = tf.get_variable(name='W4', shape=[3, 3, 48, 64], initializer=tf.contrib.layers.xavier_initializer())
-            b4 = tf.get_variable(name='b4', shape=[64], initializer=tf.zeros_initializer)
+            a_w_fc1 = tf.get_variable(name="act_W_fc1", shape=[1152, 1164],
+                                      initializer=tf.contrib.layers.xavier_initializer())
+            a_b_fc1 = tf.get_variable(name="act_b_fc1", shape=[1164],
+                                      initializer=tf.contrib.layers.xavier_initializer())
 
-            w5 = tf.get_variable(name='W5', shape=[3, 3, 64, 64], initializer=tf.contrib.layers.xavier_initializer())
-            b5 = tf.get_variable(name='b5', shape=[64], initializer=tf.zeros_initializer)
+            a_w_fc2 = tf.get_variable(name="act_W_fc2", shape=[1164, 100],
+                                      initializer=tf.contrib.layers.xavier_initializer())
+            a_b_fc2 = tf.get_variable(name="act_b_fc2", shape=[100],
+                                      initializer=tf.contrib.layers.xavier_initializer())
 
-            w_fc1 = tf.get_variable(name="W_fc1", shape=[1152, 1164],
-                                    initializer=tf.contrib.layers.xavier_initializer())
-            b_fc1 = tf.get_variable(name="b_fc1", shape=[1164], initializer=tf.contrib.layers.xavier_initializer())
+            a_w_fc3 = tf.get_variable(name="act_W_fc3", shape=[100, 50],
+                                      initializer=tf.contrib.layers.xavier_initializer())
+            a_b_fc3 = tf.get_variable(name="act_b_fc3", shape=[50],
+                                      initializer=tf.contrib.layers.xavier_initializer())
 
-            w_fc2 = tf.get_variable(name="W_fc2", shape=[1164, 100], initializer=tf.contrib.layers.xavier_initializer())
-            b_fc2 = tf.get_variable(name="b_fc2", shape=[100], initializer=tf.contrib.layers.xavier_initializer())
+            a_w_fc4 = tf.get_variable(name="act_W_fc4", shape=[50, 10],
+                                      initializer=tf.contrib.layers.xavier_initializer())
+            a_b_fc4 = tf.get_variable(name="act_b_fc4", shape=[10],
+                                      initializer=tf.contrib.layers.xavier_initializer())
 
-            w_fc3 = tf.get_variable(name="W_fc3", shape=[100, 50], initializer=tf.contrib.layers.xavier_initializer())
-            b_fc3 = tf.get_variable(name="b_fc3", shape=[50], initializer=tf.contrib.layers.xavier_initializer())
-
-            w_fc4 = tf.get_variable(name="W_fc4", shape=[50, 10], initializer=tf.contrib.layers.xavier_initializer())
-            b_fc4 = tf.get_variable(name="b_fc4", shape=[10], initializer=tf.contrib.layers.xavier_initializer())
-
-            w_fc5 = tf.get_variable(name="W_fc5", shape=[10, conf.OUTPUT_SIZE],
-                                    initializer=tf.contrib.layers.xavier_initializer())
-            b_fc5 = tf.get_variable(name="b_fc5", shape=[conf.OUTPUT_SIZE],
-                                    initializer=tf.contrib.layers.xavier_initializer())
-        with tf.name_scope("Batch_Norm"):
-            inp_batchnorm = tf.contrib.layers.batch_norm(inp, center=True, scale=True, is_training=True)
-        with tf.name_scope("Conv_Layers"):
-            conv1 = tf.nn.relu(tf.nn.conv2d(inp_batchnorm, w1, strides=[1, 2, 2, 1], padding='VALID') + b1)
-            conv2 = tf.nn.relu(tf.nn.conv2d(conv1, w2, strides=[1, 2, 2, 1], padding='VALID') + b2)
-            conv3 = tf.nn.relu(tf.nn.conv2d(conv2, w3, strides=[1, 2, 2, 1], padding='VALID') + b3)
-            conv4 = tf.nn.relu(tf.nn.conv2d(conv3, w4, strides=[1, 1, 1, 1], padding='VALID') + b4)
-            conv5 = tf.nn.relu(tf.nn.conv2d(conv4, w5, strides=[1, 1, 1, 1], padding='VALID') + b5)
-        with tf.name_scope("Dense_Layers"):
+            a_w_fc5 = tf.get_variable(name="act_W_fc5", shape=[10, env.action_space.n],
+                                      initializer=tf.contrib.layers.xavier_initializer())
+            a_b_fc5 = tf.get_variable(name="act_b_fc5", shape=[env.action_space.n],
+                                      initializer=tf.contrib.layers.xavier_initializer())
+        with tf.variable_scope("actor_conv_layers"):
+            inp_batchnorm = tf.contrib.layers.batch_norm(state_inp, center=True, scale=True, is_training=True)
+            conv1 = tf.nn.relu(tf.nn.conv2d(inp_batchnorm, a_w1, strides=[1, 2, 2, 1], padding='VALID') + a_b1)
+            conv2 = tf.nn.relu(tf.nn.conv2d(conv1, a_w2, strides=[1, 2, 2, 1], padding='VALID') + a_b2)
+            conv3 = tf.nn.relu(tf.nn.conv2d(conv2, a_w3, strides=[1, 2, 2, 1], padding='VALID') + a_b3)
+            conv4 = tf.nn.relu(tf.nn.conv2d(conv3, a_w4, strides=[1, 1, 1, 1], padding='VALID') + a_b4)
+            conv5 = tf.nn.relu(tf.nn.conv2d(conv4, a_w5, strides=[1, 1, 1, 1], padding='VALID') + a_b5)
+        with tf.name_scope("actor_dense_Layers"):
             conv5_flattened = tf.contrib.layers.flatten(conv5)
-            fc1 = tf.nn.relu(tf.matmul(conv5_flattened, w_fc1) + b_fc1)
-            fc1 = tf.nn.dropout(fc1, keep_prob=keep_prob)
-            fc2 = tf.nn.relu(tf.matmul(fc1, w_fc2) + b_fc2)
-            fc2 = tf.nn.dropout(fc2, keep_prob=keep_prob)
-            fc3 = tf.nn.relu(tf.matmul(fc2, w_fc3) + b_fc3)
-            fc3 = tf.nn.dropout(fc3, keep_prob=keep_prob)
-            fc4 = tf.nn.relu(tf.matmul(fc3, w_fc4) + b_fc4)
-            fc4 = tf.nn.dropout(fc4, keep_prob=keep_prob)
-
-        with tf.name_scope("Predictions"):
-            out = tf.nn.softsign(tf.matmul(fc4, w_fc5) + b_fc5, name="out")
-            action = tf.reduce_sum(tf.multiply(out, max_action), axis=1, name="action")
+            fc1 = tf.nn.relu(tf.matmul(conv5_flattened, a_w_fc1) + a_b_fc1)
+            fc1 = tf.nn.dropout(fc1, keep_prob=conf.keep_prob)
+            fc2 = tf.nn.relu(tf.matmul(fc1, a_w_fc2) + a_b_fc2)
+            fc2 = tf.nn.dropout(fc2, keep_prob=conf.keep_prob)
+            fc3 = tf.nn.relu(tf.matmul(fc2, a_w_fc3) + a_b_fc3)
+            fc3 = tf.nn.dropout(fc3, keep_prob=conf.keep_prob)
+            fc4 = tf.nn.relu(tf.matmul(fc3, a_w_fc4) + a_b_fc4)
+            fc4 = tf.nn.dropout(fc4, keep_prob=conf.keep_prob)
+        with tf.name_scope("actor_predictions"):
+            out = tf.nn.softsign(tf.matmul(fc4, a_w_fc5) + a_b_fc5, name="actor_output")
             tf.summary.histogram('outputs', out)
-            tf.summary.histogram('actions', action)
-        with tf.name_scope("Loss"):
-            loss = tf.reduce_mean(tf.square(action - optimal_action), name="sse_loss")
-            tf.summary.scalar('loss', loss)
-
-        with tf.name_scope("Optimizer"):
-            global_step = tf.get_variable(name="global_step", shape=[], trainable=False,
-                                          initializer=tf.zeros_initializer())
-            lr = tf.train.exponential_decay(conf.learning_rate,
-                                            global_step,
-                                            conf.decay_steps,
-                                            conf.anneal_factor,
-                                            staircase=True)
-            optimizer = tf.train.AdamOptimizer(lr).minimize(loss, global_step=global_step, name="optim")
-    print("Done creating graph")
-    graph_nodes = {"inp": inp,
-                   "max_action": max_action,
-                   "optimal_action": optimal_action,
+        with tf.name_scope("actor_loss"):
+            loss = tf.reduce_mean(tf.square(out - actor_action), name="sse_loss")
+            tf.summary.scalar("actor_loss", loss)
+        with tf.name_scope("actor_optimizer"):
+            optim = tf.train.AdamOptimizer().minimize(loss)
+    actor_nodes = {"state_inp": state_inp,
+                   "action_inp": actor_action,
                    "out": out,
-                   "action": action,
                    "loss": loss,
-                   "optimizer": optimizer}
+                   "optim": optim}
+    return state_inp, actor_nodes
 
-    return graph, graph_nodes  # inp, max_action, optimal_action, out, action, loss, optimizer
 
-
-def supervised_train(graph, nodes):
+def supervised_train(nodes):
     print("inside supervised")
     max_epochs = conf.epochs
     batch_size = conf.batch_size
-    with tf.Session(graph=graph) as sess:
+    with tf.Session() as sess:
         for epoch in range(1, max_epochs + 1):
             input_tensor = utils.get_batches()
 
 
-def deep_q_train(graph, nodes):
+def deep_q_train(nodes):
     """
     Main training loop to train the graph
     :param graph:
@@ -143,7 +136,7 @@ def deep_q_train(graph, nodes):
     :return:
     """
     keep_training = True
-    with tf.Session(graph=graph) as sess:
+    with tf.Session() as sess:
         input_tensor = np.ones((conf.img_h, conf.img_w, conf.img_d))
         inp_t = np.stack((input_tensor, input_tensor, input_tensor, input_tensor), axis=0)
         saver = tf.train.Saver()
@@ -175,9 +168,9 @@ def main():
     graph, nodes = create_graph()
     if conf.is_training:
         if conf.training_phase == 1:
-            supervised_train(graph, nodes)
+            supervised_train(nodes)
         if conf.training_phase == 2:
-            deep_q_train(graph, nodes)
+            deep_q_train(nodes)
 
 
 if __name__ == "__main__":
