e2d772cb9fdc76d1fddd0401fe8e5699755dfaba
dorozco101 <orozco.david101@gmail.com>
2018-03-14 18:03:33 -0700

    Update train_marIO_torch.py

--- train_marIO_torch.py
+++ train_marIO_torch.py
@@ -10,6 +10,47 @@ from collections import deque
 from sklearn.model_selection import train_test_split
 import pickle as pkl
 import os
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 24, 5,stride=2,bias=True)
+        self.conv2 = nn.Conv2d(24, 36, 5,stride=2,bias=True)
+        self.conv3 = nn.Conv2d(36,48,5,stride=2,bias=True)
+        self.conv4 = nn.Conv2d(48,64,3,stride=1,bias=True)
+        self.conv5 = nn.Conv2d(64,64,3,stride=1,bias=True)
+        
+        #1152, 1164
+        self.fc1 = nn.Linear(1152, 1164,bias=True)
+        self.fc2 = nn.Linear(1164, 100,bias=True)
+        self.fc3 = nn.Linear(100, 50,bias=True)
+        self.fc4 = nn.Linear(50, 10,bias=True)
+        self.fc5 = nn.Linear(10,6,bias=True)
+        
+
+    def forward(self, x):
+        x = (F.relu(self.conv1(x)))
+        x = (F.relu(self.conv2(x)))
+        x = (F.relu(self.conv3(x)))
+        x = (F.relu(self.conv4(x)))
+        x = (F.relu(self.conv5(x)))
+        x = x.view(-1, 1152)
+        
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        x = F.relu(self.fc5(x))
+        x = self.fc3(x)
+        return x
+   
 print("Using TensorFlow version: " + str(tf.__version__))
 print("This code was developed in version: 1.6.0")
 # Load Configs
